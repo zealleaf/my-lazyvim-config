f0fafaceaabe222d8b2a12513cd90104562a5094
zealleaf <github@zealleaf.me>
2025-03-06 22:30:15 +0800

    feat: some change

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "solarized-osaka",
+        colorscheme = "catppuccin-mocha",
         news = {
           lazyvim = true,
           neovim = true,
